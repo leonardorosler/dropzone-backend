ccd59e71ad89b54c399ef7a6004d99f3ca8f1848
Leonardo Rosler <roslerleonardo@gmail.com>
2026-09-10 21:42:46 -0300

    fix: instala adapter do prisma para corrigir conexao com o banco

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,5 +1,9 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/index.js";
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const prisma = new PrismaClient();
-
+export const prisma = new PrismaClient({ adapter });
