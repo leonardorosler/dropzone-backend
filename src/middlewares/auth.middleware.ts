4ac260175db3f185cf33bdb0f6a87a82263c1045
Leonardo Rosler <roslerleonardo@gmail.com>
2026-09-14 00:12:46 -0300

    feat: adiciona middleware JWT

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface TokenPayLoad{
+    sub: string,
+    role: string
+}
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      mensgem: "Token não informado",
+    });
+  }
+    const [tipo, token] = authHeader.split(" ");
+
+  if (tipo !== "Bearer" || !token) {
+    return res.status(401).json({
+      mensagem: "Token inválido",
+    });
+  }
+
+  if (!process.env.JWT_SECRET) {
+    return res.status(500).json({
+      mensagem: "JWT_SECRET não configurado",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) as TokenPayLoad;
+
+    req.usuarioId = decoded.sub;
+    req.usuarioRole = decoded.role;
+
+    return next()
+
+  } catch (error) {
+    return res.status(401).json({
+      mensagem: "Token inválido",
+    });
+  }
+}
