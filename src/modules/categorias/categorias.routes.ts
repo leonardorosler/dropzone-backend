6aa2371b0e72fe519ccab614b4d5173fa22f9b83
brend <brendontailor040@gmail.com>
2026-09-24 19:50:55 -0300

    feat: adiciona CRUD de rotas e categorias

--- src/modules/categorias/categorias.routes.ts
+++ src/modules/categorias/categorias.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 // import { listarCategoria } from "./categorias.service.js";
-import { criarCategoriaController, listarCategoriaController } from "./categorias.controller.js"
-import { authMiddleware, adminMiddleware } from "../../middlewares/auth.middleware.js"; 
+import { atualizarCategoriaController, criarCategoriaController, deletarCategoriaController, listarCategoriaController } from "./categorias.controller.js"
+import { authMiddleware } from "../../middlewares/auth.middleware.js"; 
+import { adminMiddleware } from "../../middlewares/admin.middleware .js";
 
 export const categoriasRoutes = Router();
 
 categoriasRoutes.get("/", listarCategoriaController); //feito
 categoriasRoutes.post("/", authMiddleware, adminMiddleware, criarCategoriaController); // feito
+categoriasRoutes.put("/:id", authMiddleware, adminMiddleware, atualizarCategoriaController); // feito
+categoriasRoutes.delete("/:id", authMiddleware, adminMiddleware, deletarCategoriaController); ///feito 
