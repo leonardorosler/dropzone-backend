fd3679c05c8cdc6a46e422a57fd844236c72312a
brend <brendontailor040@gmail.com>
2026-09-24 20:09:36 -0300

    feat: adiciona CRUD de favoritos

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { categoriasRoutes } from "./modules/categorias/categorias.routes.js"
 import { produtosRoutes } from "./modules/produtos/produtos.routes.js"
 import { usuariosRoutes } from "./modules/usuarios/usuarios.routes.js"
 import { authRoutes } from "./modules/auth/auth.routes.js"
+import { favoritosRoutes } from "./modules/favoritos/favoritos.routes.js"
 
 
 export const app = express()
@@ -20,3 +21,4 @@ app.use("/categorias", categoriasRoutes)
 app.use("/produtos", produtosRoutes);
 app.use("/usuarios", usuariosRoutes)
 app.use("/auth", authRoutes);
+app.use("/favoritos", favoritosRoutes);
